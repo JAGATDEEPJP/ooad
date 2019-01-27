476bdd8fa76d7edc9b931e5dccd03f41a43b0bd3
wompowe <utkarsh.agrawal@rediff.com>
2019-01-27 23:08:02 +0530

    changed mysql username and password to my system

--- src/main/java/flipkart/ooad/database/DatabaseConfiguration.java
+++ src/main/java/flipkart/ooad/database/DatabaseConfiguration.java
@@ -2,8 +2,8 @@ package flipkart.ooad.database;
 
 public class DatabaseConfiguration {
 	public static String dataBaseUrl = "jdbc:mysql://localhost:3306/FLIPKART?useSSL=false";
-	public static String userName = "root";
-	public static String password = "root";
+	public static String userName = "ooad";
+	public static String password = "ooad";
 	public DatabaseConfiguration() {
 		
 	}
