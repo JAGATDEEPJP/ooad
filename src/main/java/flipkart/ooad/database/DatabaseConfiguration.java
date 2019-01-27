76142204cb6f60304805dbc11103fe23d52a5c27
wompowe <utkarsh.agrawal@rediff.com>
2019-01-27 22:36:27 +0530

    changed name of database

--- src/main/java/flipkart/ooad/database/DatabaseConfiguration.java
+++ src/main/java/flipkart/ooad/database/DatabaseConfiguration.java
@@ -1,7 +1,7 @@
 package flipkart.ooad.database;
 
 public class DatabaseConfiguration {
-	public static String dataBaseUrl = "jdbc:mysql://localhost:3306/OOAD?useSSL=false";
+	public static String dataBaseUrl = "jdbc:mysql://localhost:3306/FLIPKART?useSSL=false";
 	public static String userName = "root";
 	public static String password = "root";
 	public DatabaseConfiguration() {
